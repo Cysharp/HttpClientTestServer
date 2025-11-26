2d8a44d5cc07a983bea539b3e6c810f606c1788b
Mayuki Sawatari <mayuki+github@misuzilla.org>
2025-11-26 12:43:41 +0900

    Adds command line configuration
    Enables configuration of the test server via command line arguments.
    This includes options for specifying the protocol (HTTP/1.1, HTTP/2),
    port, and whether to use HTTPS.
    
    Removes the launch browser setting from launchSettings.json.
    Adds a commandOptions profile for running h1/h2 with secure connection.

--- src/HttpClientTestServer/Program.cs
+++ src/HttpClientTestServer/Program.cs
@@ -1,9 +1,18 @@
-﻿using HttpClientTestServer.Endpoint;
+﻿using System.CommandLine;
+using HttpClientTestServer.Endpoint;
 using HttpClientTestServer.Services;
 using HttpClientTestServer.SessionState;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (!TryConfigureFromCommandLine(args, builder))
+{
+    Environment.ExitCode = -1;
+    return;
+}
+
+builder.Logging.AddSimpleConsole(options => options.SingleLine = true);
 builder.Services.AddGrpc();
 builder.Services.AddResponseCompression();
 
@@ -39,3 +48,47 @@ app.MapGet("/compression", () => Results.Content(new string('A', 1024 * 64))); /
 app.MapGet("/accept-encodings", (HttpContext httpContext) => Results.Content(string.Join(",", httpContext.Request.Headers.AcceptEncoding.ToArray())));
 
 app.Run();
+
+
+static bool TryConfigureFromCommandLine(string[] args, WebApplicationBuilder builder)
+{
+    var rootCommand = new RootCommand();
+    var optionProtocolVersion = new Option<HttpProtocols?>("--protocol");
+    var optionPort = new Option<int?>("--port", "-p");
+    var optionSecure = new Option<bool>("--secure", "-s");
+    rootCommand.Options.Add(optionProtocolVersion);
+    rootCommand.Options.Add(optionPort);
+    rootCommand.Options.Add(optionSecure);
+
+    var result = rootCommand.Parse(args);
+    if (result.Errors.Any())
+    {
+        foreach (var error in result.Errors)
+        {
+            Console.Error.WriteLine($"Error: {error.Message}");
+        }
+        return false;
+    }
+
+    var port = result.GetValue(optionPort);
+    if (port is not null)
+    {
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(port.Value, listenOptions =>
+            {
+                if (result.GetValue(optionSecure))
+                {
+                    listenOptions.UseHttps();
+                    listenOptions.Protocols = result.GetValue(optionProtocolVersion) ?? HttpProtocols.Http1AndHttp2;
+                }
+                else
+                {
+                    listenOptions.Protocols = result.GetValue(optionProtocolVersion) ?? HttpProtocols.Http1;
+                }
+            });
+        });
+    }
+
+    return true;
+}
