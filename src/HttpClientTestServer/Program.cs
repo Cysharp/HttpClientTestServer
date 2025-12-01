3cd83922987126ef157cede4e99227f69472e2bd
Mayuki Sawatari <mayuki@misuzilla.org>
2025-12-01 12:27:09 +0900

    Server as a Library

--- src/HttpClientTestServer/Program.cs
+++ src/HttpClientTestServer/Program.cs
@@ -1,68 +1,26 @@
-﻿using HttpClientTestServer.ConnectionState;
-using HttpClientTestServer.Endpoint;
-using HttpClientTestServer.Services;
-using HttpClientTestServer.SessionState;
+﻿using HttpClientTestServer;
+using HttpClientTestServer.ConnectionState;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using System.Collections.Concurrent;
 using System.CommandLine;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
-builder.Logging.AddSimpleConsole(options => options.SingleLine = true);
-builder.Services.AddConnectionState();
-builder.Services.AddSessionState();
-builder.Services.AddGrpc();
-builder.Services.AddResponseCompression(options =>
+await using var server = new ServerApplication(args);
+server.ConfigureBuilder(builder =>
 {
-    options.EnableForHttps = true;
+    builder.Logging.AddSimpleConsole(options => options.SingleLine = true);
 });
 
-if (!TryConfigureFromCommandLine(args, builder))
+if (!TryConfigureFromCommandLine(args, server))
 {
     Environment.ExitCode = -1;
     return;
 }
 
-var app = builder.Build();
+await server.StartAsync();
+await server.Stopped;
 
-// ConnectionId header
-app.Use((ctx, next) =>
-{
-    ctx.Response.Headers["x-connection-id"] = ctx.Connection.Id;
-    return next(ctx);
-});
-
-// SessionState
-app.UseMiddleware<SessionStateMiddleware>();
-app.MapSessionState();
-
-// ConnectionState
-app.MapConnectionState();
-
-// HTTP/1 and HTTP/2
-app.MapAppEndpoints();
-
-// gRPC
-app.MapGrpcService<GreeterService>();
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    RequestPath = "/_certs",
-    FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(Path.Combine(AppContext.BaseDirectory, "Certificates")),
-    ServeUnknownFileTypes = true,
-});
-
-// Compression
-app.UseResponseCompression();
-app.MapGet("/compression", () => Results.Content(new string('A', 1024 * 64))); // 64KB
-app.MapGet("/accept-encodings", (HttpContext httpContext) => Results.Content(string.Join(",", httpContext.Request.Headers.AcceptEncoding.ToArray())));
-
-app.Run();
-
-
-static bool TryConfigureFromCommandLine(string[] args, WebApplicationBuilder builder)
+static bool TryConfigureFromCommandLine(string[] args, ServerApplication server)
 {
     var rootCommand = new RootCommand();
     var optionProtocolVersion = new Option<HttpProtocols?>("--protocol") { Description = "HTTP protocol version" };
@@ -84,30 +42,33 @@ static bool TryConfigureFromCommandLine(string[] args, WebApplicationBuilder bui
     var port = result.GetValue(optionPort);
     if (port is not null)
     {
-#pragma warning disable ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
-        var logger = builder.Logging.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
-#pragma warning restore ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
-        builder.WebHost.ConfigureKestrel(options =>
+        server.ConfigureBuilder(builder =>
         {
-            options.ConfigureHttpsDefaults(options =>
+#pragma warning disable ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
+            var logger = builder.Logging.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+#pragma warning restore ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
+            builder.WebHost.ConfigureKestrel(options =>
             {
-                options.ServerCertificate = X509CertificateLoader.LoadPkcs12FromFile(Path.Combine(AppContext.BaseDirectory, "Certificates", "localhost.pfx"), null);
-            });
-            options.ListenAnyIP(port.Value, listenOptions =>
-            {
-                var isSecure = result.GetValue(optionSecure);
-                var protocols = result.GetValue(optionProtocolVersion) ?? (isSecure ? HttpProtocols.Http1AndHttp2 : HttpProtocols.Http1);
-                var sslProtocols = result.GetValue(optionTlsVersion) ?? SslProtocols.None;
-                logger.LogInformation("Configuring server on port {Port} (Secure: {Secure}, Protocol: {Protocol}, SslProtocols: {SslProtocols})", port.Value, isSecure, protocols, sslProtocols);
-                if (isSecure)
+                options.ConfigureHttpsDefaults(options =>
                 {
-                    listenOptions.UseHttps(options =>
+                    options.ServerCertificate = X509CertificateLoader.LoadPkcs12FromFile(Path.Combine(AppContext.BaseDirectory, "Certificates", "localhost.pfx"), null);
+                });
+                options.ListenAnyIP(port.Value, listenOptions =>
+                {
+                    var isSecure = result.GetValue(optionSecure);
+                    var protocols = result.GetValue(optionProtocolVersion) ?? (isSecure ? HttpProtocols.Http1AndHttp2 : HttpProtocols.Http1);
+                    var sslProtocols = result.GetValue(optionTlsVersion) ?? SslProtocols.None;
+                    logger.LogInformation("Configuring server on port {Port} (Secure: {Secure}, Protocol: {Protocol}, SslProtocols: {SslProtocols})", port.Value, isSecure, protocols, sslProtocols);
+                    if (isSecure)
                     {
-                        options.SslProtocols = sslProtocols;
-                    });
-                }
-                listenOptions.Protocols = protocols;
-                listenOptions.UseConnectionState();
+                        listenOptions.UseHttps(options =>
+                        {
+                            options.SslProtocols = sslProtocols;
+                        });
+                    }
+                    listenOptions.Protocols = protocols;
+                    listenOptions.UseConnectionState();
+                });
             });
         });
     }
---
+++ src/HttpClientTestServer/ServerApplication.cs
@@ -0,0 +1,130 @@
+﻿using HttpClientTestServer.ConnectionState;
+using HttpClientTestServer.Endpoint;
+using HttpClientTestServer.Services;
+using HttpClientTestServer.SessionState;
+
+namespace HttpClientTestServer;
+
+public class ServerApplication : IAsyncDisposable
+{
+    private Task? _runningTask;
+    private readonly WebApplicationBuilder builder;
+    private readonly List<Action<WebApplicationBuilder>> _configureBuilders = new();
+    private readonly List<Action<WebApplication>> _configureApps = new();
+    private WebApplication? _application;
+    private IHostApplicationLifetime? _appLifetime;
+    private readonly TaskCompletionSource _waitForAppStarted = new();
+    private readonly TaskCompletionSource _appStopped = new();
+
+    public Task Stopped => _appStopped.Task;
+
+    public ServerApplication(string[] args)
+    {
+        builder = WebApplication.CreateSlimBuilder(args);
+    }
+
+    private void ConfigureBuilderDefaults()
+    {
+        builder.Services.AddConnectionState();
+        builder.Services.AddSessionState();
+        builder.Services.AddGrpc();
+        builder.Services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+        });
+    }
+
+    private void ConfigureAppDefaults()
+    {
+        var app = _application ?? throw new InvalidOperationException();
+
+        // ConnectionId header
+        app.Use((ctx, next) =>
+        {
+            ctx.Response.Headers["x-connection-id"] = ctx.Connection.Id;
+            return next(ctx);
+        });
+
+        // SessionState
+        app.UseMiddleware<SessionStateMiddleware>();
+        app.MapSessionState();
+
+        // ConnectionState
+        app.MapConnectionState();
+
+        // HTTP/1 and HTTP/2
+        app.MapAppEndpoints();
+
+        // gRPC
+        app.MapGrpcService<GreeterService>();
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            RequestPath = "/_certs",
+            FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(Path.Combine(AppContext.BaseDirectory, "Certificates")),
+            ServeUnknownFileTypes = true,
+        });
+
+        // Compression
+        app.UseResponseCompression();
+        app.MapGet("/compression", () => Results.Content(new string('A', 1024 * 64))); // 64KB
+        app.MapGet("/accept-encodings", (HttpContext httpContext) => Results.Content(string.Join(",", httpContext.Request.Headers.AcceptEncoding.ToArray())));
+
+    }
+
+    public void ConfigureForConsoleMode()
+    {
+        builder.Logging.AddSimpleConsole(options => options.SingleLine = true);
+    }
+
+    public void ConfigureBuilder(Action<WebApplicationBuilder> configure)
+    {
+        _configureBuilders.Add(configure);
+    }
+
+    public async Task StartAsync()
+    {
+        ConfigureBuilderDefaults();
+
+        foreach (var c in _configureBuilders)
+        {
+            c(builder);
+        }
+
+        _application = builder.Build();
+        
+        ConfigureAppDefaults();
+
+        foreach (var c in _configureApps)
+        {
+            c(_application);
+        }
+
+        _runningTask = _application.RunAsync();
+        if (_runningTask.IsFaulted)
+        {
+            await _runningTask;
+        }
+
+        _appLifetime = _application.Services.GetRequiredService<IHostApplicationLifetime>();
+        _appLifetime.ApplicationStarted.Register(() => _waitForAppStarted.SetResult());
+        _appLifetime.ApplicationStopped.Register(() => _appStopped.SetResult());
+
+        await _waitForAppStarted.Task;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_appLifetime is not null && _runningTask is not null)
+        {
+            _appLifetime.StopApplication();
+            try
+            {
+                await _runningTask.WaitAsync(TimeSpan.FromSeconds(1));
+            }
+            catch (TimeoutException)
+            {
+            }
+        }
+    }
+}
